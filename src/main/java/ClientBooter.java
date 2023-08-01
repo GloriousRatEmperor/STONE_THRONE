74256ff3e8f7d96966f7fbef2eca731c37ae7a85
GloriousRatEmperor <franta.stehlik.cz@gmail.com>
2023-08-01 11:13:41 +0200

    saasd
    asddass

--- src/main/java/ClientBooter.java
+++ src/main/java/ClientBooter.java
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
+
 import Multiplayer.Client;
 import Multiplayer.ClientData;
 import Multiplayer.ServerData;
-=======
->>>>>>> 244325ca06514a2b144c81d9ea80a7fe7b3a59ac
+
 import Multiplayer.TechnicalClient;
 import jade.Window;
 
@@ -17,14 +16,11 @@ public class ClientBooter {
         BlockingQueue<ServerData> responses=new ArrayBlockingQueue<>(150);
         Thread.UncaughtExceptionHandler h = (th, ex) -> System.out.println("Uncaught exception: " + ex);
         String adress=args[0];
-<<<<<<< HEAD
 
         Thread clientThread= new Thread(new TechnicalClient(adress,requests,responses));
         clientThread.setUncaughtExceptionHandler(h);
         clientThread.start();
-=======
-        new TechnicalClient(adress);
->>>>>>> 244325ca06514a2b144c81d9ea80a7fe7b3a59ac
+
 
         Window window = Window.get();
         window.clientThread=clientThread;
